e831517a649763e98ef716574954e0580dcf9000
Rachel S. Lee <rslee0025@gmail.com>
2020-06-04 00:13:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+  def prime?(integer)
+    integer = []
+    if integer.any? {|i| i.prime?}
+      return
+    else 
+      false
+    end
+  end  
+ 
+ 
+ 
+
+def prime?(n)
+  (2..n-1).none? {|divisor| n % divisor == 0}
+end
