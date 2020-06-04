31e9dba1ed63d96ecf8ebc23c332ce0b356ac800
Rachel S. Lee <rslee0025@gmail.com>
2020-06-04 00:15:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,12 @@
-  def prime?(integer)
-    integer = []
-    if integer.any? {|i| i.prime?}
-      return
-    else 
-      false
+def prime?(number)
+  start = 2
+  if number > 1
+    range = (start..number-1).to_a
+    range.none? do |num_to_test| #none of the numbers should return true for none? to return true. If none of the numbers
+                                # return true, then they are not divisible by the number and the number is prime
+      number % num_to_test == 0
     end
-  end  
- 
- 
- 
-
-def prime?(n)
-  (2..n-1).none? {|divisor| n % divisor == 0}
+  else
+    false
+  end
 end
